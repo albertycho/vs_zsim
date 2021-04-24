f20ce845b3ee5a0a9194b705e8966acb3e11998f
Albert Cho <albertycho90@gmail.com>
2021-04-24 20:39:57 +0900

    e

--- src/sim_nic.h
+++ src/sim_nic.h
@@ -9,6 +9,54 @@
 #ifndef _SIM_NIC_H_
 #define _SIM_NIC_H_
 
+
+#define MAX_NUM_WQ 8
+
+#define NICELEM 		nicInfo->nic_elem[procIdx]
+#define NWQ_VAL 		nicInfo->nic_elem[procIdx].wq_valid
+#define NCQ_VAL 		nicInfo->nic_elem[procIdx].cq_valid
+
+#define NWQ_HEAD 		nicInfo->nic_elem[procIdx].wq_head
+#define NWQ_TAIL 		nicInfo->nic_elem[procIdx].wq_tail
+#define NCQ_HEAD 		nicInfo->nic_elem[procIdx].cq_head
+#define NCQ_TAIL 		nicInfo->nic_elem[procIdx].cq_tail
+
+#define NWQ_Q 			nicInfo->nic_elem[procIdx].wq
+#define NCQ_Q 			nicInfo->nic_elem[procIdx].cq
+
+
+#define NICELEM_P(a) 	nicInfo->nic_elem[a]
+#define NWQ_val_P(a) 	nicInfo->nic_elem[a].wq_valid
+#define NCQ_VAL_P(a)	nicInfo->nic_elem[a].cq_valid
+
+#define NWQ_HEAD_P(a) 	nicInfo->nic_elem[a].wq_head
+#define NWQ_TAIL_P(a) 	nicInfo->nic_elem[a].wq_tail
+#define NCQ_HEAD_P(a) 	nicInfo->nic_elem[a].cq_head
+#define NCQ_TAIL_P(a) 	nicInfo->nic_elem[a].cq_tail
+
+#define NWQ_Q_P(a)		nicInfo->nic_elem[a].wq
+#define NCQ_Q_P(a)		nicInfo->nic_elem[a].cq
+
+
+#define RMC_READ                1
+#define RMC_WRITE               2
+#define RMC_RMW                 3
+#define RMC_SABRE	            4
+#define RMC_RECV                5
+#define RMC_MSG_NACK            6
+
+#define SEND_OP_SHIFT           4
+#define PAIRED_SEND_SHIFT       0
+#define INCOMING_SEND_SHIFT     1
+#define INCOMING_RESP_SHIFT     2
+#define RMC_SEND                (1<<SEND_OP_SHIFT)
+#define RMC_PAIRED_SEND         (1<<SEND_OP_SHIFT) | (1<<PAIRED_SEND_SHIFT)
+#define RMC_INCOMING_SEND       (1<<SEND_OP_SHIFT) | (1<<INCOMING_SEND_SHIFT)
+#define RMC_INCOMING_RESP       (1<<SEND_OP_SHIFT) | (1<<INCOMING_RESP_SHIFT)
+#define RMC_INVAL               42
+
+#define RECV_BUF_POOL_SIZE 100
+
 typedef struct wq_entry {
 	//first double-word (8 bytes)
 	uint32_t op;        //up to 64 soNUMA ops
