9ceb4d35c51ba8760fe6cb7ae599c3c6932d3b14
Albert Cho <albertycho90@gmail.com>
2021-04-25 03:20:12 +0900

    e

--- src/sim_nic.h
+++ src/sim_nic.h
@@ -15,7 +15,7 @@
 #define _SIM_NIC_H_
 
 
-extern glob_nic_elements* nicInfo;
+glob_nic_elements* nicInfo;
 
 void init_nicInfo();
 int create_cq_event(uint32_t procIdx, bool SR, uint32_t success, uint32_t tid, uint64_t recv_buf_addr);
