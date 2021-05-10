0ec674fb1c7d99e55e0c32addbc2005e9207c7b2
Albert Cho <albertycho90@gmail.com>
2021-05-10 14:06:06 +0900

    e

--- tests/compile_tests.sh
+++ tests/compile_tests.sh
@@ -17,3 +17,6 @@
 echo "compile_tests.sh: rebuild tests"
 rm qp_test
 g++ qp_test.cpp zsim_nic_defines.cpp -std=c++11 -o qp_test
+
+rm loop_test
+g++ loop_test.cpp zsim_nic_defines.cpp -std=c++11 -o loop_test
---
+++ tests/loop_test.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <cstdint>
+#include <stdlib.h>
+#include <stdint.h>
+#include "zsim_nic_defines.hpp"
+
+using namespace std;
+
+int main() {
+
+	rmc_cq_t * cq;
+
+	register_buffer((void*) (&cq), (void*) 1);
+
+	//std::cout<<"APP: wq="<<std::hex<<wq<<std::endl;
+
+	uint64_t count = 0;
+
+	//while(send_count<=32)
+	while (cq->q[0].valid == false) {
+		count++;
+	}
+
+	std::cout << "APP: count=" << count << std::endl;
+	//test how long bbl will go
+
+	uint64_t cq_rbuf_addr = cq->q[0].recv_buf_addr;
+	uint64_t tid = cq->q[0].tid;
+	count += tid;
+	count += cq_rbuf_addr;
+	count = count + count;
+
+	register_buffer((void*) 0, (void*) 0xdead);
+	std::cout<<"APP - terminating"<<std::endl;
+    return 0;
+}
