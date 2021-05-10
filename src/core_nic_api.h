11a5ad8ba47acf4489b6a8b94a3fb74f8cfcb463
Albert Cho <albertycho90@gmail.com>
2021-05-10 21:52:42 +0900

    e

--- src/core_nic_api.h
+++ src/core_nic_api.h
@@ -178,7 +178,8 @@ uint64_t RRPP_allocate_recv_buf(uint32_t blen, glob_nic_elements* nicInfo, uint3
 
 int inject_inbound_packet(int message, uint64_t recv_buf_addr) { //input is packet, so type may change with code update
 	//write to recv buffer TODO: do this in a function?
-	info("recv_buf_addr:%d", recv_buf_addr);
+	//info("recv_buf_addr:%d", recv_buf_addr);
+	std::cout << "recv_buf_addr:" << std::hex << recv_buf_addr << std::endl;
 	*((uint64_t*)recv_buf_addr) = message;
 	info("writing message to recv buffer successfully");
 	//update uarch state (call access)
