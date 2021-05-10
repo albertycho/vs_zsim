eb66333032f9f8ce3162904b65e208d47d283fe6
Albert Cho <albertycho90@gmail.com>
2021-05-10 19:55:29 +0900

    e

--- src/timing_core.cpp
+++ src/timing_core.cpp
@@ -27,7 +27,7 @@
 #include "filter_cache.h"
 #include "zsim.h"
 
-#include "core_nic_api.h"
+//#include "core_nic_api.h"
 
 #include <thread>
 
@@ -96,6 +96,8 @@ void TimingCore::storeAndRecord(Address addr) {
 void TimingCore::bblAndRecord(Address bblAddr, BblInfo* bblInfo) {
     instrs += bblInfo->instrs;
     curCycle += bblInfo->instrs;
+    
+    /*
     glob_nic_elements* nicInfo = static_cast<glob_nic_elements*>(gm_get_nic_ptr());
     void* lg_p = static_cast<void*>(gm_get_lg_ptr());
     
@@ -120,7 +122,7 @@ void TimingCore::bblAndRecord(Address bblAddr, BblInfo* bblInfo) {
     core_ceq_routine(curCycle, nicInfo, 0);
     RRPP_routine(curCycle, nicInfo, lg_p, 0);
     RCP_routine(curCycle, nicInfo, 0);
-
+    */
     Address endBblAddr = bblAddr + bblInfo->bytes;
     for (Address fetchAddr = bblAddr; fetchAddr < endBblAddr; fetchAddr+=(1 << lineBits)) {
         uint64_t startCycle = curCycle;
