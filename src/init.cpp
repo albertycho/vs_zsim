c4a9644c92bf9017e8c71f69eaead3feed65786c
Albert Cho <albertycho90@gmail.com>
2021-05-10 21:05:32 +0900

    e

--- src/init.cpp
+++ src/init.cpp
@@ -1033,6 +1033,7 @@ void SimInit(const char* configFile, const char* outputDir, uint32_t shmid) {
 
     void* lgp;
     lgp = gm_calloc<load_generator>();
+    lgp->next_cycle = 100000;
     gm_set_lg_ptr(lgp);
   
 	//TODO: remove test_tag after validation
