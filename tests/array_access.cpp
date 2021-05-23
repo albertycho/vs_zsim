87d6e4f44e86c9745d7a92109182711f8da52825
Albert Cho <albertycho90@gmail.com>
2021-05-23 19:01:42 +0900

    e

---
+++ tests/array_access.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <cstdint>
+#include <stdlib.h>
+#include <stdint.h>
+#include "zsim_nic_defines.hpp"
+
+#define ARR_SIZE 100000
+using namespace std;
+
+int main() {
+
+
+	rmc_wq_t * wq;
+	rmc_cq_t * cq;
+
+	uint32_t * lbuf_base;
+	uint32_t * lbuf_ptr;
+	//register lbuf_base
+
+	register_buffer((void*) (&lbuf_base), (void*) 2);
+	register_buffer((void*) (&wq), (void*) 0);
+	register_buffer((void*) (&cq), (void*) 1);
+
+	uint64_t long_array[ARR_SIZE];
+
+	for (int i = 0; i < ARR_SIZE; i++) {
+		long_array[i] = i;
+	}
+
+	uint64_t sum = 0;
+	for (int i = 0; i < ARR_SIZE; i++) {
+		sum += long_array[i];
+	}
+	std::cout << "sum=" << sum << std::endl;
+	return 0;
+
+}
--- tests/compile_tests.sh
+++ tests/compile_tests.sh
@@ -1,18 +1,6 @@
 #!/bin/sh
 
-#rm buffer_test
-#rm buffer_test_2
-#g++ buffer_test.cpp -std=c++11 -o buffer_test
-#g++ buffer_test2.cpp -std=c++11 -o buffer_test_2
-#rm wqcq_test
-#rm wqcq_test2
-#g++ wqcq_test.cpp -std=c++11 -o wqcq_test
-#g++ wqcq_test2.cpp -std=c++11 -o wqcq_test2
-#
-#rm buffer_test_2
-#rm buffer_test
-#g++ buffer_test.cpp -std=c++11 -o buffer_test
-#g++ buffer_test2.cpp -std=c++11 -o buffer_test_2
+
 
 echo "compile_tests.sh: rebuild tests"
 rm qp_test
@@ -20,3 +8,6 @@ g++ qp_test.cpp zsim_nic_defines.cpp -std=c++11 -o qp_test
 
 rm loop_test
 g++ loop_test.cpp zsim_nic_defines.cpp -std=c++11 -o loop_test
+
+rm control_test
+g++ array_access.cpp zsim_nic_defines.cpp -std=c++11 -o control_test
