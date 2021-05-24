fcc0696ec1569d018bc8c592732c07a9514cfa43
Albert Cho <albertycho90@gmail.com>
2021-05-25 01:07:57 +0900

    e

--- src/ooo_core.cpp
+++ src/ooo_core.cpp
@@ -515,7 +515,7 @@ void OOOCore::BblFunc(THREADID tid, ADDRINT bblAddr, BblInfo* bblInfo) {
                     for (uint64_t i = 0; i < RECV_BUF_POOL_SIZE; i++) {
                         uint64_t recv_buf_addr = (uint64_t)(&(nicInfo->nic_elem[procIdx].recv_buf[i]));
                         nicInfo->nic_elem[procIdx].recv_buf[i] = i;
-                        l1d->store(recv_buf_addr, core->curCycle);
+                        core->l1d->store(recv_buf_addr, core->curCycle);
 
                     }
                 
