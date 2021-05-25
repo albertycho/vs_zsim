51b93fa051af94c498beaec6f9d0be49c41ae775
Albert Cho <albertycho90@gmail.com>
2021-05-25 21:30:56 +0900

    e

--- src/ooo_core.cpp
+++ src/ooo_core.cpp
@@ -518,7 +518,7 @@ void OOOCore::BblFunc(THREADID tid, ADDRINT bblAddr, BblInfo* bblInfo) {
                         nicInfo->nic_elem[procIdx].recv_buf[i] = i;
                         //uint64_t reqSatisfiedCycle = l1d->store(addr, dispatchCycle) + L1D_LAT;
                         uint64_t reqSatisfiedCycle = core->l1d->store(recv_buf_addr, core->curCycle)+ L1D_LAT;
-                        core->cRec.record(core->curCycle, core->curCycle, reqSatisfiedCycle);
+                        //core->cRec.record(core->curCycle, core->curCycle, reqSatisfiedCycle);
                     }
                 
             }
