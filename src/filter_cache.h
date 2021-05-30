22046e6a67b61aff4ba21b9533065f81802ac2b0
Albert Cho <albertycho90@gmail.com>
2021-05-30 15:19:38 +0900

    e

--- src/filter_cache.h
+++ src/filter_cache.h
@@ -148,6 +148,44 @@ class FilterCache : public Cache {
             return respCycle;
         }
 
+        // NO_RECORD versions of load/store/replace
+        uint64_t replace_norecord(Address vLineAddr, uint32_t idx, bool isLoad, uint64_t curCycle) {
+            Address pLineAddr = procMask | vLineAddr;
+            MESIState dummyState = MESIState::I;
+            futex_lock(&filterLock);
+            uint32_t nr_flags = reqFlags || NORECORD; //1<<6
+            MemReq req = { pLineAddr, isLoad ? GETS : GETX, 0, &dummyState, curCycle, &filterLock, dummyState, srcId, reqFlags };
+            uint64_t respCycle = access(req);
+
+            //Due to the way we do the locking, at this point the old address might be invalidated, but we have the new address guaranteed until we release the lock
+
+            //Careful with this order
+            Address oldAddr = filterArray[idx].rdAddr;
+            filterArray[idx].wrAddr = isLoad ? -1L : vLineAddr;
+            filterArray[idx].rdAddr = vLineAddr;
+
+            //For LSU simulation purposes, loads bypass stores even to the same line if there is no conflict,
+            //(e.g., st to x, ld from x+8) and we implement store-load forwarding at the core.
+            //So if this is a load, it always sets availCycle; if it is a store hit, it doesn't
+            if (oldAddr != vLineAddr) filterArray[idx].availCycle = respCycle;
+
+            futex_unlock(&filterLock);
+            return respCycle;
+        }
+
+        inline uint64_t load_norecord(Address vAddr, uint64_t curCycle) {
+            Address vLineAddr = vAddr >> lineBits;
+            uint32_t idx = vLineAddr & setMask;
+            uint64_t availCycle = filterArray[idx].availCycle; //read before, careful with ordering to avoid timing races
+            if (vLineAddr == filterArray[idx].rdAddr) {
+                fGETSHit++;
+                return MAX(curCycle, availCycle);
+            }
+            else {
+                return replace_norecord(vLineAddr, idx, true, curCycle);
+            }
+        }
+
         uint64_t invalidate(const InvReq& req) {
             Cache::startInvalidate();  // grabs cache's downLock
             futex_lock(&filterLock);
