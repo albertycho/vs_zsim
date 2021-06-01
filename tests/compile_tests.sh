21090f07a8ec1f647551a5c2a4567d48605c4cf9
Albert Cho <albertycho90@gmail.com>
2021-06-02 01:17:26 +0900

    e

--- tests/compile_tests.sh
+++ tests/compile_tests.sh
@@ -3,11 +3,14 @@
 
 
 echo "compile_tests.sh: rebuild tests"
-rm qp_test
-g++ qp_test.cpp zsim_nic_defines.cpp -std=c++11 -o qp_test
+#rm qp_test
+#g++ qp_test.cpp zsim_nic_defines.cpp -std=c++11 -o qp_test
 
-rm loop_test
-g++ loop_test.cpp zsim_nic_defines.cpp -std=c++11 -o loop_test
+#rm loop_test
+#g++ loop_test.cpp zsim_nic_defines.cpp -std=c++11 -o loop_test
 
 rm control_test
 g++ array_access.cpp zsim_nic_defines.cpp -std=c++11 -o control_test
+
+rm control_test_no_locality
+g++ array_access_large.cpp zsim_nic_defines.cpp -std=c++11 -o control_test_no_locality
