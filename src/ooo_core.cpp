5e895f97a646d6578d2e3c8d63f102bcc395ed44
Albert Cho <albertycho90@gmail.com>
2021-06-09 23:43:56 +0900

    e

--- src/ooo_core.cpp
+++ src/ooo_core.cpp
@@ -533,7 +533,7 @@ void OOOCore::BblFunc(THREADID tid, ADDRINT bblAddr, BblInfo* bblInfo) {
                 int srcId = getCid(tid);
 
                 MemReq req = { rbuf_lineAddr, GETX, 0xDA0000, &dummyState, core->curCycle, NULL, dummyState, srcId, 0 };
-                uint64_t reqSatisfiedCycle = core->l1d->getParent(recv_buf_addr >> lineBits)->access(req);
+                uint64_t reqSatisfiedCycle = core->l1d->getParent(rbuf_addr>> lineBits)->access(req);
                 //std::cout << core->l1d->getParent(recv_buf_addr >> lineBits)->getName() << std::endl;
                 //assert((!core->cRec.getEventRecorder()->hasRecord()));
                 core->cRec.record(core->curCycle, core->curCycle, reqSatisfiedCycle);
