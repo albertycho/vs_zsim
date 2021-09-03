8bdc7d2b101945e92c9dc4c0d696c2d9743a2eb1
albertycho90 <albertycho90@gmail.com>
2021-09-03 08:39:45 -0400

    e

--- src/decoder.cpp
+++ src/decoder.cpp
@@ -231,17 +231,6 @@ void Decoder::emitXchg(Instr& instr, DynUopVec& uops) {
         assert(instr.numLoads == 1 && instr.numStores == 1);
         assert(instr.numInRegs == 1 && instr.numOutRegs == 1);
         assert(instr.inRegs[0] == instr.outRegs[0]);
-        
-        //UNTESTED CODE
-        // WIP but just commenting out for commit before traveling
-        // if (rbx, rbx)
-        /*
-        info("emitXchg - mem - reg");
-        for (int i = 0; i < MAX_INSTR_REG_READS; i++) {
-            info("inRegs[%d] = %d", i, instr.inRegs[i]);
-        }
-        */
-
 
         emitLoad(instr, 0, uops);
         emitExecUop(instr.inRegs[0], 0, REG_EXEC_TEMP, 0, uops, 1, PORTS_015); //r -> temp
@@ -250,9 +239,14 @@ void Decoder::emitXchg(Instr& instr, DynUopVec& uops) {
         if (!INS_LockPrefix(instr.ins)) emitFence(uops, 14); //xchg has an implicit lock prefix (TODO: Check we don't introduce two fences...)
     } else { // reg <-> reg
         // WIP but just commenting out for commit before traveling
+
         
-        info("emitXchg - reg - reg");
+        //info("emitXchg - reg - reg");
+        
+
         if (INS_IsXchg(instr.ins) && INS_OperandReg(instr.ins, 0) == REG_RBX && INS_OperandReg(instr.ins, 1) == REG_RBX) {
+            // don't emit UOP's for nic related instructions
+            /* DBGP
             info("xchg rbx rbx seen at decoder");
             for (int i = 0; i < MAX_INSTR_REG_READS; i++) {
                 info("inRegs[%d] = %d", i, instr.inRegs[i]);
@@ -260,24 +254,18 @@ void Decoder::emitXchg(Instr& instr, DynUopVec& uops) {
             for (int i = 0; i < MAX_INSTR_REG_READS; i++) {
                 info("outRegs[%d] = %d", i, instr.outRegs[i]);
             }
+            */
         }
-        /*
-        for (int i = 0; i < MAX_INSTR_REG_READS; i++) {
-            info("inRegs[%d] = %d", i, instr.inRegs[i]);
-        }
-        for (int i = 0; i < MAX_INSTR_REG_READS; i++) {
-            info("outRegs[%d] = %d", i, instr.outRegs[i]);
-        }
-        */
 
-        assert(instr.numInRegs == 2 && instr.numOutRegs == 2);
-        assert(instr.inRegs[0] == instr.outRegs[0]);
-        assert(instr.inRegs[1] == instr.outRegs[1]);
+        else {
+            assert(instr.numInRegs == 2 && instr.numOutRegs == 2);
+            assert(instr.inRegs[0] == instr.outRegs[0]);
+            assert(instr.inRegs[1] == instr.outRegs[1]);
 
-        emitExecUop(instr.inRegs[0], 0, REG_EXEC_TEMP, 0, uops, 1, PORTS_015);
-        emitExecUop(instr.inRegs[1], 0, instr.outRegs[0], 0, uops, 1, PORTS_015);
-        emitExecUop(REG_EXEC_TEMP, 0, instr.outRegs[1], 0, uops, 1, PORTS_015);
-        // emit_wq_notify? probably not needed
+            emitExecUop(instr.inRegs[0], 0, REG_EXEC_TEMP, 0, uops, 1, PORTS_015);
+            emitExecUop(instr.inRegs[1], 0, instr.outRegs[0], 0, uops, 1, PORTS_015);
+            emitExecUop(REG_EXEC_TEMP, 0, instr.outRegs[1], 0, uops, 1, PORTS_015);
+        }
     }
 }
 
