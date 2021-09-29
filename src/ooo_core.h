d017793663d13225607742a0e77b136ebe139ff9
albertycho90 <albertycho90@gmail.com>
2021-09-29 10:30:43 -0400

    e

--- src/ooo_core.h
+++ src/ooo_core.h
@@ -518,7 +518,7 @@ class OOOCore : public Core {
         static void BblFunc(THREADID tid, ADDRINT bblAddr, BblInfo* bblInfo);
         static void BranchFunc(THREADID tid, ADDRINT pc, BOOL taken, ADDRINT takenNpc, ADDRINT notTakenNpc);
         static void NicMagicFunc(THREADID tid, ADDRINT val, ADDRINT field);
-        static int  nic_ingress_routine();
+        static int  nic_ingress_routine(THREADID tid);
 } ATTR_LINE_ALIGNED;  // Take up an int number of cache lines
 
 #endif  // OOO_CORE_H_
