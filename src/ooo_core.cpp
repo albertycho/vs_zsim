479750798ee32cc1889b1616667427b38f21d4ac
Albert Y Cho <acho44@lambic.cc.gatech.edu>
2021-10-15 14:27:02 -0400

    e

--- src/ooo_core.cpp
+++ src/ooo_core.cpp
@@ -471,12 +471,15 @@ void OOOCore::leave() {
 }
 
 void OOOCore::cSimStart() {
+    //dbgprint
+    info("CSimStart called");
     uint64_t targetCycle = cRec.cSimStart(curCycle);
     assert(targetCycle >= curCycle);
     if (targetCycle > curCycle) advance(targetCycle);
 }
 
 void OOOCore::cSimEnd() {
+    info("CSimEnd called");
     uint64_t targetCycle = cRec.cSimEnd(curCycle);
     assert(targetCycle >= curCycle);
     if (targetCycle > curCycle) advance(targetCycle);
