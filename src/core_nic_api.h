6ddbc459101029722a669897bfdd97ea2632b080
albertycho90 <albertycho90@gmail.com>
2021-11-02 12:52:51 -0400

    e

--- src/core_nic_api.h
+++ src/core_nic_api.h
@@ -411,7 +411,7 @@ int inject_incoming_packet(uint64_t& cur_cycle, glob_nic_elements* nicInfo, void
 	futex_lock(&nicInfo->nic_elem[core_id].rb_lock);
 	uint32_t rb_head = allocate_recv_buf(8, nicInfo, core_id);
 	//dbgprint
-	//info("allocate_recv_buf - rb_head = %d", rb_head);
+	info("allocate_recv_buf - rb_head = %d", rb_head);
 
 	futex_unlock(&nicInfo->nic_elem[core_id].rb_lock);
 	if (rb_head > RECV_BUF_POOL_SIZE) {
