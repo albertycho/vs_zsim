c0deff484174f8e10c4fa13c666534e2e93d1c69
albertycho90 <albertycho90@gmail.com>
2021-11-15 17:31:39 -0500

    e

--- src/ooo_core.h
+++ src/ooo_core.h
@@ -491,6 +491,7 @@ class OOOCore : public Core {
         uint16_t ingr_type, egr_type, egr_inval;
         uint32_t cycle_adj_queue[100000]; // curCycle at start of cSimStart() and at end of cSimEnd()
         uint32_t start_cnt_phases = 0;
+        uint32_t cycle_adj_idx = 0;
 
     private:
         inline void load(Address addr);
