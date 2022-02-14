9adb366448020c4fd81e4897fd513d0ac933c09e
Albert <albertycho90@gmail.com>
2022-02-14 15:20:38 -0500

    e

--- src/ddr_mem.cpp
+++ src/ddr_mem.cpp
@@ -294,7 +294,7 @@ void DDRMemory::EstimateBandwidth() {
 
     lastAccesses = totalAccesses;
 
-    info("zsim_phases since last mem BW sampling: %d",zinfo->numPhases - lastPhase);
+    info("zsim_phases since last mem BW sampling: %d, curBW:: %d",zinfo->numPhases - lastPhase, curBandwidth);
     lastPhase = zinfo->numPhases;
 
 
