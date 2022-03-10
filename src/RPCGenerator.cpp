1a2b7df301f032e0ed79fe305b34b46e6e935959
Albert <albertycho90@gmail.com>
2022-03-09 19:47:27 -0500

    e

--- src/RPCGenerator.cpp
+++ src/RPCGenerator.cpp
@@ -207,11 +207,13 @@ RPCGenerator::generatePackedRPC(char* userBuffer, uint32_t packet_size) const {
 		/* Create the ipv6 exact match flow */
 		memset(&entry, 0, sizeof(entry));
 		entry = ipv6_l3fwd_em_route_array[port];
-		for(int j=0; j<IPV6_ADDR_LEN-1;j++){
-			uint64_t a=key_i>>(j*2);
-			uint8_t b =a&3;
-			entry.key.ip_dst[j]+=b;
-			entry.key.ip_src[j]+=b;
+			if(key_i>63){
+			for(int j=0; j<IPV6_ADDR_LEN-1;j++){
+				uint64_t a=key_i>>(j*2);
+				uint8_t b =a&3;
+				entry.key.ip_dst[j]+=b;
+				entry.key.ip_src[j]+=b;
+			}
 		}
 		//entry.key.ip_dst[15] = (key_i + 1) % 256;//BYTE_VALUE_MAX;
 		entry.key.ip_dst[15] = (port+1) % 256;//BYTE_VALUE_MAX;
